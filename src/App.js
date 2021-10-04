5b44a35138d5ff8b88491e0195ed78f257f6524c
tomerkeisar <businesssites1@gmail.com>
2021-10-04 13:58:27 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { ToastContainer } from 'react-toastify';
+
+import SinglePageAppComp from "./components/navigation/SinglePageAppComp";
+function App(){
+return <div className="App">
+    <>
+<SinglePageAppComp></SinglePageAppComp>
+    <ToastContainer></ToastContainer>
+</>
+</div>
+}
+
+export default App;
